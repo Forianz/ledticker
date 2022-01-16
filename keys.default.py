536f3162404095ff7f8c36a3767df56c889d533c
Forianz <93485651+Forianz@users.noreply.github.com>
2022-01-16 11:20:38 +0100

    Update keys.default.py

--- keys.default.py
+++ keys.default.py
@@ -1,2 +1,2 @@
-coinmarketcap_key = 'YOUR_KEY' # pro.coinmarketcap.com
-owm_key = 'YOUR_KEY' # openweathermap.org/appid
+coinmarketcap_key = '43d11b7d-542c-4390-88d4-59ffa9c03440' # pro.coinmarketcap.com
+owm_key = '56fc4e1906e6c9104c9d8d1d03bc9b87' # openweathermap.org/appid
